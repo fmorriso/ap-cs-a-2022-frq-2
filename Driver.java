7587c38c9a8dbb206894d4ba36028cfd20d817f5
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2022-05-07 14:34:34 -0600

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,27 @@
+/**
+ * <b>Purpose:</b> A driver that will run both part A and part B of the FRQ 
+ * @author Fred Morrison
+ *
+ */
+public class Driver
+{
+
+	public static void main(String[] args)
+	{
+		PartA();
+		PartB();
+	}
+
+	private static void PartA()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void PartB()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
